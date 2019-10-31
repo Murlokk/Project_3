4588b33c4bde408334ccaadd231fac58fbbc65a9
Alexa Ciprian <alexaciprian302@yahoo.ro>
2019-10-31 14:44:16 +0200

    Media aritmetica a n numere Aplicatia va cere prima data la carte numere vrei sa faci media aritmetica, dupa care va cere intriducerea fiecarui numar in parte si la sfarsit va afisa media aritmetica a acelor numere

---
+++ Media aritmetica a n numere/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_aritmetica_a_n_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumarulIntrodus;
+            int n, Numarator = 1;
+            float MediaAritmetica, Suma = 0;
+            Console.WriteLine("Introduce numarul de numere pe care doriti sa aflati media aritmetica :");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (Numarator <= n)
+            {
+                Console.WriteLine("Inroduceti numarul :" + Numarator + "");
+                NumarulIntrodus = Convert.ToInt32(Console.ReadLine());
+                Suma += NumarulIntrodus;
+                ++Numarator;
+            }
+            MediaAritmetica = (float)Suma / (n);
+            Console.WriteLine("\nMedia aritmetica este");
+            Console.Write(MediaAritmetica);
+        }
+    }
+}
